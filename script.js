3b9a839a111007ec647cd4ffb79a7123dbdc456b
JustRhidwan <107209973+JustRhidwan@users.noreply.github.com>
2025-10-20 20:52:19 +0100

    updaing

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ updateTime();
 //  Continuous updates: Update the time every 1000 milliseconds (1 second)
 setInterval(updateTime, 1000); 
 
+const form = document.querySelector('form');
+const fullname = document.querySelector('.name').value.trim;
+const email = document.querySelector('.email');
+const emailError = document.querySelector('#email-error');
+const subject = document.querySelector('.subject');
+const submit = document.querySelector('button');
+
+email.addEventListener('input', (event)=> {
+    if(email.validity.valid) {
+        emailError.textContent="dff";
+    } 
+    else{
+
+    }
+})
